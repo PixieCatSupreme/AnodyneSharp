33f8a8393a90ea2bcfd695c930c322b23a11b399
PixieCatSupreme <drawolf999@gmail.com>
2020-07-01 11:01:22 +0200

    Added Windows/ DirectX version
    - Changed windmill_blade to be two rows (textures this wide make DirectX crash on load)

---
+++ AnodyneSharp/AnodyneSharp.Windows/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnodyneSharp.Windows
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new AnodyneGame())
+                game.Run();
+        }
+    }
+}
