00f615afc215056ff0160fa8cfafe648cf53e6e5
Seph De Busser <seph.debusser@gmail.com>
2020-07-05 17:46:07 +0200

    Added entity split on type/frames and example Dev Easter egg

--- AnodyneSharp/AnodyneSharp.Shared/Entities/Base/NamedEntity.cs
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Base/NamedEntity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace AnodyneSharp.Entities
 {
@@ -6,10 +7,14 @@ namespace AnodyneSharp.Entities
     sealed class NamedEntity : Attribute
     {
         readonly string xmlName;
+        public readonly string type;
+        public readonly int[] frames;
 
-        public NamedEntity(string xmlName = null)
+        public NamedEntity(string xmlName = null, string type = null, params int[] frames)
         {
             this.xmlName = xmlName;
+            this.type = type;
+            this.frames = frames;
         }
 
         public string GetName(Type type)
@@ -21,6 +26,11 @@ namespace AnodyneSharp.Entities
             {
                 return type.Name;
             }
+        }
+
+        public bool Matches(int f, string t)
+        {
+            return (type ?? t) == t && (frames.Length == 0 || frames.Contains(f));
         }
     }
 }
---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/DevEaster.cs
@@ -0,0 +1,34 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Drawing;
+using AnodyneSharp.Registry;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc
+{
+    [NamedEntity("NPC", "generic", 17, 18), Collision(typeof(Player))]
+    class DevEaster : Entity, Interactable
+    {
+        string scene;
+
+        public DevEaster(EntityPreset preset, Player p) : base(preset.Position, "dev_npcs", 16,16, DrawOrder.ENTITIES)
+        {
+            immovable = true;
+            scene = preset.Frame == 17 ? "melos" : "marina";
+            SetFrame(preset.Frame == 17 ? 0 : 10);
+        }
+
+        public override void Collided(Entity other)
+        {
+            Separate(other, this);
+        }
+
+        public bool PlayerInteraction(Facing player_direction)
+        {
+            GlobalState.Dialogue = DialogueManager.GetDialogue("generic_npc", "DEBUG", scene, 0);
+            return true;
+        }
+    }
+}
