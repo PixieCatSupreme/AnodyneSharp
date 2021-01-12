346f7bebcc519fcfb5df27e88efed55e3de02a94
Seph De Busser <seph.debusser@gmail.com>
2021-01-12 17:33:26 +0100

    Added Statue

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/Statue.cs
@@ -0,0 +1,30 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Drawing;
+using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc
+{
+    [NamedEntity("NPC","statue"), Collision(typeof(Player))]
+    public class Statue : Entity, Interactable
+    {
+        public Statue(EntityPreset preset, Player p)
+            : base(preset.Position, "sage_statue", 16, 16, DrawOrder.ENTITIES)
+        {
+            immovable = true;
+        }
+
+        public override void Collided(Entity other)
+        {
+            Separate(other, this);
+        }
+
+        public bool PlayerInteraction(Facing player_direction)
+        {
+            GlobalState.Dialogue = DialogueManager.GetDialogue("statue", "one");
+            return true;
+        }
+    }
+}
