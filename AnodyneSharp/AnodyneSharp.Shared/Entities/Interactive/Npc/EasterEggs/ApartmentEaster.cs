d5eeebcf6d9590f4972e4167737097ed0d40c55c
PixieCatSupreme <drawolf999@gmail.com>
2021-08-23 19:32:54 +0200

    Added ApartmentEaster

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/EasterEggs/ApartmentEaster.cs
@@ -0,0 +1,35 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc.EasterEggs
+{
+    [NamedEntity("NPC", type: "generic", 8), Collision(typeof(Player))]
+    class ApartmentEaster : Entity, Interactable
+    {
+        public ApartmentEaster(EntityPreset preset, Player p)
+            : base(preset.Position, "randoms", 16, 16, Drawing.DrawOrder.ENTITIES)
+        {
+            AddAnimation("APT", CreateAnimFrameArray(0));
+
+            Play("APT");
+
+            immovable = true;
+        }
+
+        public bool PlayerInteraction(Facing player_direction)
+        {
+            GlobalState.Dialogue = DialogueManager.GetDialogue("generic_npc", "easter_egg");
+
+            return true;
+        }
+
+        public override void Collided(Entity other)
+        {
+            base.Collided(other);
+            Separate(this, other);
+        }
+    }
+}
