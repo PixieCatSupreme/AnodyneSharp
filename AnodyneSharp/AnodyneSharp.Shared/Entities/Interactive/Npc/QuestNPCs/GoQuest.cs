76cef43c4caa0b7b60fa417edc9ce01ec119fc93
PixieCatSupreme <drawolf999@gmail.com>
2021-12-27 22:23:14 +0100

    Added the last Quest NPCs

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/QuestNPCs/GoQuest.cs
@@ -0,0 +1,41 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc.QuestNPCs
+{
+    [NamedEntity("NPC", type: "generic", map: "GO", 7), Collision(typeof(Player))]
+    class GoQuest : Entity, Interactable
+    {
+        bool _played_quest = false;
+
+        public GoQuest(EntityPreset preset, Player p)
+            : base(preset.Position, "note_rock", 16, 16, Drawing.DrawOrder.ENTITIES)
+        {
+            AddAnimation("a", CreateAnimFrameArray(0));
+
+            Play("a");
+            immovable = true;
+        }
+
+        public override void Collided(Entity other)
+        {
+            base.Collided(other);
+            Separate(this, other);
+        }
+
+        public bool PlayerInteraction(Facing player_direction)
+        {
+            int quest_progress = GlobalState.events.GetEvent("GoQuestProgress");
+            if ((quest_progress == 7) && !_played_quest)
+            {
+                _played_quest = true;
+                GlobalState.Dialogue = DialogueManager.GetDialogue("generic_npc", "quest_event");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/QuestNPCs/SpaceQuest.cs
@@ -0,0 +1,45 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc.QuestNPCs
+{
+    [NamedEntity("NPC", type: "generic", map: "SPACE", 7), Collision(typeof(Player))]
+    class SpaceQuest : Entity, Interactable
+    {
+        bool _played_quest = false;
+
+        public SpaceQuest(EntityPreset preset, Player p)
+            : base(preset.Position, "space_npcs", 16, 16, Drawing.DrawOrder.ENTITIES)
+        {
+            AddAnimation("a", CreateAnimFrameArray(0, 10,1,11), 10, true);
+
+            Play("a");
+            immovable = true;
+        }
+
+        public override void Collided(Entity other)
+        {
+            base.Collided(other);
+            Separate(this, other);
+        }
+
+        public bool PlayerInteraction(Facing player_direction)
+        {
+            int quest_progress = GlobalState.events.GetEvent("GoQuestProgress");
+            if ((quest_progress == 6 || quest_progress == 7) && !_played_quest)
+            {
+                _played_quest = true;
+                GlobalState.Dialogue = DialogueManager.GetDialogue("generic_npc", "quest_event");
+                if (quest_progress == 6) GlobalState.events.IncEvent("GoQuestProgress");
+            }
+            else
+            {
+                GlobalState.Dialogue = DialogueManager.GetDialogue("generic_npc", "quest_normal");
+            }
+            return true;
+        }
+    }
+}
