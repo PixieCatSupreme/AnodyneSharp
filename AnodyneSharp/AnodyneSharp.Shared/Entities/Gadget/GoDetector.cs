7e72339b4eb9de361e6ae3707d0ebb980718d056
Seph De Busser <seph.debusser@gmail.com>
2021-12-30 21:03:18 +0100

    Added GoDetector

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Gadget/GoDetector.cs
@@ -0,0 +1,41 @@
+﻿using AnodyneSharp.Registry;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Gadget
+{
+    [NamedEntity("Go_Detector")]
+    class GoDetector : Entity
+    {
+        List<Point> checks;
+        EntityPreset _preset;
+        public GoDetector(EntityPreset preset, Player p) : base(preset.Position,Drawing.DrawOrder.BACKGROUND)
+        {
+            visible = false;
+            _preset = preset;
+            checks = preset.TypeValue.Split(';').Select((s) =>
+            {
+                int[] vals = s.Split(',').Select(int.Parse).ToArray();
+                return new Point(vals[0], vals[1]);
+            }).ToList();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            Point b = GlobalState.Map.ToMapLoc(Position);
+            for(int i = 0; i < checks.Count; ++i)
+            {
+                if(GlobalState.Map.GetTile(MapData.Layer.BG,b+checks[i]) != 60+i)
+                {
+                    return;
+                }
+            }
+            GlobalState.PUZZLES_SOLVED++;
+            _preset.Alive = exists = false;
+        }
+    }
+}
