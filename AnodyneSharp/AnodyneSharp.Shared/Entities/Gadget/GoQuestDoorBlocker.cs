15a8cce3938b192ea7491137a3c6176553e39fd0
Seph De Busser <seph.debusser@gmail.com>
2021-12-31 11:12:23 +0100

    Added GoQuestDoorBlocker

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Gadget/GoQuestDoorBlocker.cs
@@ -0,0 +1,29 @@
+﻿using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Gadget
+{
+    [NamedEntity]
+    class GoQuestDoorBlocker : Entity
+    {
+        Events.DoorToggle doorBlocker;
+
+        public GoQuestDoorBlocker(EntityPreset preset, Player p) : base(preset.Position,Drawing.DrawOrder.BACKGROUND)
+        {
+            visible = false;
+            doorBlocker = new(preset.Position, 16, 16);
+            exists = false;
+        }
+
+        public override IEnumerable<Entity> SubEntities()
+        {
+            if (GlobalState.events.GetEvent("GoQuestProgress") < 7)
+            {
+                return new List<Entity>() { doorBlocker };
+            }
+            return base.SubEntities();
+        }
+    }
+}
