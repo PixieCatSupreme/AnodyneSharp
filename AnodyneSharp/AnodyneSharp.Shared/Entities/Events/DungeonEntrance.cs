954456e4f12a1900e8c13be2ed2fa7046fc05b1c
Seph De Busser <seph.debusser@gmail.com>
2022-01-02 22:47:58 +0100

    Linked together hotel boss stages and dungeon entrances

--- AnodyneSharp/AnodyneSharp.Shared/Entities/Events/DungeonEntrance.cs
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Events/DungeonEntrance.cs
@@ -3,11 +3,12 @@ using AnodyneSharp.Registry;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AnodyneSharp.Entities
 {
-    [NamedEntity("Event", null, 5)]
+    [NamedEntity("Event", "entrance", 5)]
     public class DungeonEntrance : Entity
     {
         public DungeonEntrance(EntityPreset preset, Player p) : base(preset.Position, DrawOrder.ENTITIES)
@@ -17,6 +18,17 @@ namespace AnodyneSharp.Entities
             if (GlobalState.ReturnTarget.map == GlobalState.CURRENT_MAP_NAME) return;
             
             GlobalState.ReturnTarget = new(GlobalState.CURRENT_MAP_NAME, preset.Position - new Vector2(10, 34));
+        }
+    }
+
+    [NamedEntity("Event", "entrance_ref", 5)]
+    public class DungeonEntranceRef : Entity
+    {
+        public DungeonEntranceRef(EntityPreset preset, Player p) : base(preset.Position, DrawOrder.ENTITIES)
+        {
+            exists = false;
+
+            _ = new DungeonEntrance(EntityManager.GetLinkGroup(preset.LinkID).Where(s => s.TypeValue == "entrance").First(),p);
         }
     }
 }
