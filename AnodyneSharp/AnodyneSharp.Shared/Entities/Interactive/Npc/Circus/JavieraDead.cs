7efcb60e02ec6189da3797aa2bc5ed5052b22205
PixieCatSupreme <drawolf999@gmail.com>
2022-01-03 16:47:05 +0100

    Added dead circus folks

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/Circus/JavieraDead.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc.Circus
+{
+    [NamedEntity("Circus_Folks", "dead", 1)]
+    class JavieraDead : Entity
+    {
+        public JavieraDead(EntityPreset preset, Player p)
+            : base(preset.Position, "javiera", 16, 16, Drawing.DrawOrder.ENTITIES)
+        {
+            SetFrame(12);
+        }
+    }
+}
