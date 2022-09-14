4868104964b6ef4f5fcf0110bcd6f7bce0aeff2a
SephDB <seph.debusser@gmail.com>
2022-09-14 21:06:27 +0200

    Briar: Added dust explosions

--- AnodyneSharp/AnodyneSharp.Shared/Entities/Enemy/Go/BlueThorn.cs
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Enemy/Go/BlueThorn.cs
@@ -1,7 +1,9 @@
 ﻿using AnodyneSharp.GameEvents;
 using AnodyneSharp.Registry;
+using AnodyneSharp.Sounds;
 using AnodyneSharp.Utilities;
 using Microsoft.Xna.Framework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,11 +42,14 @@ namespace AnodyneSharp.Entities.Enemy.Go
         public class FireEye : Entity
         {
             IEnumerator state;
-            EntityPool<Fireball> fireballs = new(12, () => new());
+            EntityPool<Fireball> fireballs;
             EntityPool<Dust> dust = new(4, () => new());
+            EntityPool<DustExplosion> explosions = new(4, ()=>new());
+
 
             public FireEye() : base(Vector2.Zero,"briar_fire_eye",16,16,Drawing.DrawOrder.BG_ENTITIES)
             {
+                fireballs = new(12, () => new(this));
                 AddAnimation("shoot", CreateAnimFrameArray(6, 7, 8, 9), 10);
                 AddAnimation("grow", CreateAnimFrameArray(0, 1, 2, 3, 4, 5), 10, false);
                 AddAnimation("ungrow", CreateAnimFrameArray(5, 4, 3, 2, 1, 0), 10, false);
@@ -64,17 +69,54 @@ namespace AnodyneSharp.Entities.Enemy.Go
                 state = CoroutineUtils.OnceEvery(() => fireballs.Spawn((f) => f.Spawn(Position + Vector2.One * 4, p.Position)), 0.4f);
             }
 
+            public void ExplodeAt(Dust d)
+            {
+                d.exists = false;
+                explosions.Spawn(e => e.Spawn(d));
+                foreach(Facing f in Enum.GetValues(typeof(Facing)))
+                {
+                    fireballs.Spawn(e => e.SpawnFacing(d.Position, f));
+                }
+            }
+
             public override void Update()
             {
                 base.Update();
                 state?.MoveNext();
             }
 
-
-
             public override IEnumerable<Entity> SubEntities()
             {
-                return fireballs.Entities.Concat(dust.Entities);
+                return fireballs.Entities.Concat(dust.Entities).Concat(explosions.Entities);
+            }
+
+        }
+
+        [Collision(typeof(BlueThorn))]
+        public class DustExplosion : Entity
+        {
+            public DustExplosion() : base(Vector2.Zero, "briar_dust_explosion", 48, 48, Drawing.DrawOrder.ENTITIES)
+            {
+                AddAnimation("explode", CreateAnimFrameArray(0, 1, 2, 3, 4, 5), 12, false);
+            }
+
+            public void Spawn(Dust d)
+            {
+                Play("explode");
+                SoundManager.PlaySoundEffect("dust_explode");
+                Position = d.Position - Vector2.One * 16;
+            }
+
+            public override void Update()
+            {
+                base.Update();
+                if (_curAnim.Finished) exists = false;
+            }
+
+            public override void Collided(Entity other)
+            {
+                base.Collided(other);
+                other.Play("hurt");
             }
 
         }
@@ -82,18 +124,27 @@ namespace AnodyneSharp.Entities.Enemy.Go
         [Collision(typeof(Player),typeof(Dust),KeepOnScreen = true)]
         public class Fireball : Entity
         {
-            public Fireball() : base(Vector2.Zero, "briar_fire_eye", 16,16, Drawing.DrawOrder.ENTITIES)
+            FireEye parent;
+
+            public Fireball(FireEye parent) : base(Vector2.Zero, "briar_fire_eye", 16, 16, Drawing.DrawOrder.ENTITIES)
             {
                 AddAnimation("move", CreateAnimFrameArray(12, 13), 12);
                 Play("move");
                 width = height = 6;
                 CenterOffset();
+                this.parent = parent;
             }
 
             public void Spawn(Vector2 pos, Vector2 target)
             {
                 Position = pos;
                 MoveTowards(target, 45);
+            }
+
+            public void SpawnFacing(Vector2 pos, Facing f)
+            {
+                Position = pos;
+                velocity = FacingDirection(f) * 70;
             }
 
             public override void Update()
@@ -113,6 +164,11 @@ namespace AnodyneSharp.Entities.Enemy.Go
                 if(other is Player p && p.state == PlayerState.GROUND)
                 {
                     p.ReceiveDamage(1);
+                }
+                else if(other is Dust d)
+                {
+                    parent.ExplodeAt(d);
+                    exists = false;
                 }
             }
         }
