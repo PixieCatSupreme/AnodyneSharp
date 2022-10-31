5e6d331a10cc557638c4206f599c2b358806d635
Seph De Busser <seph.debusser@gmail.com>
2022-10-31 20:05:47 +0100

    Added two simple entities in DEBUG

---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Decorations/BigTree.cs
@@ -0,0 +1,26 @@
+﻿using AnodyneSharp.Drawing;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Decorations
+{
+    [NamedEntity("Solid_Sprite","tree"), Collision(typeof(Player))]
+    public class BigTree : Entity
+    {
+        public BigTree(EntityPreset preset, Player p) : base(preset.Position,"TREE",64,64,DrawOrder.ENTITIES)
+        {
+            offset = new(16, 32);
+            width = height = 32;
+            Position += offset;
+            immovable = true;
+        }
+
+        public override void Collided(Entity other)
+        {
+            base.Collided(other);
+            Separate(this, other);
+        }
+    }
+}
---
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Interactive/Npc/Debug/TestNPC.cs
@@ -0,0 +1,29 @@
+﻿using AnodyneSharp.Dialogue;
+using AnodyneSharp.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodyneSharp.Entities.Interactive.Npc.Debug
+{
+    [NamedEntity("NPC","npc_test",0), Collision(typeof(Player))]
+    public class TestNPC : Entity, Interactable
+    {
+        public TestNPC(EntityPreset preset, Player p) : base(preset.Position,"npc",16,16,Drawing.DrawOrder.ENTITIES)
+        {
+            immovable = true;
+        }
+
+        public bool PlayerInteraction(Facing player_direction)
+        {
+            GlobalState.Dialogue = DialogueManager.GetDialogue("test", "scene_1");
+            return true;
+        }
+
+        public override void Collided(Entity other)
+        {
+            base.Collided(other);
+            Separate(this, other);
+        }
+    }
+}
