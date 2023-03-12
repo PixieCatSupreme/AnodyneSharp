1c654087855aa36df82bcb3c0a849a7bff452ed3
Seph De Busser <seph.debusser@gmail.com>
2023-03-12 09:22:49 +0100

    Fix draw order of nonsolid

--- AnodyneSharp/AnodyneSharp.Shared/Entities/Decorations/NonSolid.cs
+++ AnodyneSharp/AnodyneSharp.Shared/Entities/Decorations/NonSolid.cs
@@ -8,13 +8,13 @@ namespace AnodyneSharp.Entities.Decorations
     [NamedEntity("Nonsolid", "Rail_CROWD")]
     class CrowdRail : Entity
     {
-        public CrowdRail(EntityPreset preset, Player p) : base(preset.Position, "rail_CROWD", 16, 22, Drawing.DrawOrder.FG_SPRITES) { }
+        public CrowdRail(EntityPreset preset, Player p) : base(preset.Position, "rail_CROWD", 16, 22, DrawOrder.ENTITITES) { }
     }
 
     [NamedEntity("Nonsolid","Rail_1")]
     class Rail : Entity
     {
-        public Rail(EntityPreset preset, Player p) : base(preset.Position, "rail", 16, 16, DrawOrder.FG_SPRITES) { }
+        public Rail(EntityPreset preset, Player p) : base(preset.Position, "rail", 16, 16, DrawOrder.ENTITIES) { }
     }
 
 }
